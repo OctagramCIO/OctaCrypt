3f0b5e907e13b47da2c6d14145f09ec257f056f9
Octagram_Oficial <octagramoficial@proton.me>
2026-01-19 15:31:28 -0500

    Update crypto_engine.py

--- octacrypt/core/crypto_engine.py
+++ octacrypt/core/crypto_engine.py
@@ -1,1 +1,16 @@
+# octacrypt/core/crypto_engine.py
 
+from octacrypt.algorithms.aes_cipher import AESCipher
+
+class CryptoEngine:
+    def __init__(self, algorithm="AES"):
+        if algorithm == "AES":
+            self.cipher = AESCipher()
+        else:
+            raise ValueError("Algoritmo no soportado")
+
+    def encrypt(self, plaintext: str) -> dict:
+        return self.cipher.encrypt(plaintext)
+
+    def decrypt(self, encrypted_data: dict) -> str:
+        return self.cipher.decrypt(encrypted_data)
