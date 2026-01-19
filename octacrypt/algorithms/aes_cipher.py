ca109987cc5dd4d6c3617547c96e145361c05c31
Octagram_Oficial <octagramoficial@proton.me>
2026-01-19 15:32:01 -0500

    Update aes_cipher.py

--- octacrypt/algorithms/aes_cipher.py
+++ octacrypt/algorithms/aes_cipher.py
@@ -1,1 +1,20 @@
+# octacrypt/algorithms/aes_cipher.py
 
+from cryptography.fernet import Fernet
+
+class AESCipher:
+    def __init__(self):
+        self.key = Fernet.generate_key()
+        self.cipher = Fernet(self.key)
+
+    def encrypt(self, plaintext: str) -> dict:
+        encrypted = self.cipher.encrypt(plaintext.encode())
+        return {
+            "ciphertext": encrypted,
+            "key": self.key
+        }
+
+    def decrypt(self, encrypted_data: dict) -> str:
+        cipher = Fernet(encrypted_data["key"])
+        decrypted = cipher.decrypt(encrypted_data["ciphertext"])
+        return decrypted.decode()
