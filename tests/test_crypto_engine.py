d10acb1fcd964e1fa375364cabb4d1ea190826cf
Octagram_Oficial <octagramoficial@proton.me>
2026-01-19 15:50:58 -0500

    Update test_crypto_engine.py
    El nacimiento de algo muy grande

--- tests/test_crypto_engine.py
+++ tests/test_crypto_engine.py
@@ -1,1 +1,14 @@
+from octacrypt.core.crypto_engine import CryptoEngine
 
+def test_encrypt_and_decrypt():
+    engine = CryptoEngine(
+        algorithm="xor",
+        key=b"octagram"
+    )
+
+    original = b"OctaCrypt is alive"
+    encrypted = engine.encrypt(original)
+    decrypted = engine.decrypt(encrypted)
+
+    assert encrypted != original
+    assert decrypted == original
