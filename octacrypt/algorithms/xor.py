410af5c3a4db81cabdbc622e69e9e6d5cacc60de
Octagram_Oficial <octagramoficial@proton.me>
2026-01-19 15:50:13 -0500

    Update xor.py
    El inicio de algo grande

--- octacrypt/algorithms/xor.py
+++ octacrypt/algorithms/xor.py
@@ -1,1 +1,13 @@
+class XorCipher:
+    def __init__(self, key: bytes):
+        self.key = key
 
+    def encrypt(self, data: bytes) -> bytes:
+        return bytes(
+            data[i] ^ self.key[i % len(self.key)]
+            for i in range(len(data))
+        )
+
+    def decrypt(self, data: bytes) -> bytes:
+        # XOR es simétrico
+        return self.encrypt(data)
