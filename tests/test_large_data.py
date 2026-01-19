3da7aa41ee4ee21d256812f453b7d65e74d4c272
Octagram_Oficial <octagramoficial@proton.me>
2026-01-19 16:08:44 -0500

    Create test_large_data.py
    La ultima prueba antes de avanzar

---
+++ tests/test_large_data.py
@@ -0,0 +1,11 @@
+def test_large_data():
+    engine = CryptoEngine(
+        algorithm="xor",
+        key=b"octagram"
+    )
+
+    data = b"A" * 10_000
+    encrypted = engine.encrypt(data)
+    decrypted = engine.decrypt(encrypted)
+
+    assert decrypted == data
