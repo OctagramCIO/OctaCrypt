6148bfdb16d1558bdf738d3059985220652c98c7
starluciel3-netizen <octagramoficial@proton.me>
2026-01-20 21:12:06 -0500

    Ultima correccion antes de avanzar
    seguimos mejorando el codigo

--- octacrypt/core/crypto.py
+++ octacrypt/core/crypto.py
@@ -4,34 +4,41 @@ from pathlib import Path
 from octacrypt.core.crypto_engine import CryptoEngine
 
 
-def encrypt_file(input_path: Path, output_path: Path | None, key: str):
+def encrypt_file(input_path: Path, output_path: str | Path | None, key: str):
     engine = CryptoEngine(
         algorithm="xor",
         key=key.encode()
     )
 
-    data = input_path.read_bytes()
-    encrypted = engine.encrypt(data)
+    input_path = Path(input_path)
 
     if output_path is None:
         output_path = input_path.with_suffix(input_path.suffix + ".enc")
+    else:
+        output_path = Path(output_path)
+
+    data = input_path.read_bytes()
+    encrypted = engine.encrypt(data)
 
     output_path.write_bytes(encrypted)
     return output_path
 
 
-def decrypt_file(input_path: Path, output_path: Path | None, key: str):
+def decrypt_file(input_path: Path, output_path: str | Path | None, key: str):
     engine = CryptoEngine(
         algorithm="xor",
         key=key.encode()
     )
 
-    data = input_path.read_bytes()
-    decrypted = engine.decrypt(data)
+    input_path = Path(input_path)
 
     if output_path is None:
         output_path = input_path.with_suffix("")
+    else:
+        output_path = Path(output_path)
+
+    data = input_path.read_bytes()
+    decrypted = engine.decrypt(data)
 
     output_path.write_bytes(decrypted)
     return output_path
-
