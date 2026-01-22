b365773071e61b778d922638282aa18820edc12c
starluciel3-netizen <octagramoficial@proton.me>
2026-01-21 21:48:48 -0500

    Nuevo algoritmo y nuevos comienzos

---
+++ octacrypt/algorithms/aes.py
@@ -0,0 +1,26 @@
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+import os
+
+
+class AESAlgorithm:
+    NONCE_SIZE = 12  # recomendado para GCM
+
+    def __init__(self, key: bytes):
+        if not isinstance(key, bytes):
+            raise TypeError("Key must be bytes")
+
+        if len(key) != 32:
+            raise ValueError("AES key must be 32 bytes (AES-256)")
+
+        self.key = key
+        self.aes = AESGCM(self.key)
+
+    def encrypt(self, data: bytes) -> bytes:
+        nonce = os.urandom(self.NONCE_SIZE)
+        ciphertext = self.aes.encrypt(nonce, data, None)
+        return nonce + ciphertext
+
+    def decrypt(self, data: bytes) -> bytes:
+        nonce = data[:self.NONCE_SIZE]
+        ciphertext = data[self.NONCE_SIZE:]
+        return self.aes.decrypt(nonce, ciphertext, None)
